2cf119eef326d232b1e246481e1aa24c7583228f
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-06-02 15:26:04 +0900

    instant commit

--- api-user-springboot/src/main/java/com/btb/user/configuration/CORSFilter.java
+++ api-user-springboot/src/main/java/com/btb/user/configuration/CORSFilter.java
@@ -1,11 +1,14 @@
 package com.btb.user.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
 public class CORSFilter implements Filter {
-
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
