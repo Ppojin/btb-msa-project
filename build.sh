800b90253fd0630c94109074980e2ddae39f8eff
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-06-02 17:39:03 +0900

    instant commit

--- build.sh
+++ build.sh
@@ -2,34 +2,34 @@
 cd back-config-springboot
 mvn clean compile package
 ## build
-docker build -t config back-config-springboot/.
+docker build -t config .
 
 cd ../back-eureka-springboot
 mvn clean compile package
 ## build
-docker build -t eureka back-eureka-springboot/.
+docker build -t eureka .
 
 cd ../back-zuul-springboot
 mvn clean compile package
 ## build
-docker build -t zuul back-zuul-springboot/.
+docker build -t zuul .
 
 cd ../api-user-springboot
 mvn clean compile package
 ## build
-docker build -t user api-user-springboot/.
+docker build -t user .
 
 cd ../api-qabank-springboot
 mvn clean compile package
 ## build
-docker build -t qabank api-qabank-springboot/.
+docker build -t qabank .
 
 cd ../api-exam-springboot
 mvn clean compile package
 ## build
-docker build -t exam api-exam-springboot/.
+docker build -t exam .
 
 cd ../api-result-springboot
 mvn clean compile package
 ## build
-docker build -t result api-result-springboot/.
+docker build -t result .
