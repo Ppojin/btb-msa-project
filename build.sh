46f59d7dc1da3ca1b6321a82c7bf998fa6ff6ddd
ppojin <hhjin1814@gmail.com>
2020-06-13 15:16:27 +0900

    docker compose

--- build.sh
+++ build.sh
@@ -3,26 +3,31 @@ cd back-config-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/config .
+docker push ppojin/config
 
 cd ../back-eureka-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/eureka .
+docker push ppojin/eureka
 
 cd ../back-zuul-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/zuul .
+docker push ppojin/zuul
 
 cd ../api-user-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/user .
+docker push ppojin/user
 
 cd ../api-qabank-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/qabank .
+docker push ppojin/qabank
 
 # cd ../api-exam-springboot
 # mvn clean compile package
@@ -33,3 +38,4 @@ cd ../api-result-springboot
 mvn clean compile package -DskipTests
 ## build
 docker build -t ppojin/result .
+docker push ppojin/result
